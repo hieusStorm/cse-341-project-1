036f1daa5812bee0d4fe9408d313ed7345fdc597
hieusStorm <hyrum555@gmail.com>
2025-03-08 08:55:31 -0700

    fixed object creation mistake

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -10,7 +10,7 @@ const getAll = async (req, res) => {
 };
 
 const getSingle = async (req, res) => {
-    const contactId = ObjectId(req.params.id)
+    const contactId = new ObjectId(req.params.id)
     const result = await mongodb.getDatabase().db().collection('contact').find({ _id: contactId });
     result.toArray().then((contact) => {
         res.setHeader('Content-Type', 'application/json');
