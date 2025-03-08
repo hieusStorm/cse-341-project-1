86c553b8baea5adf16bf433b56ef76615d8fec79
hieusStorm <hyrum555@gmail.com>
2025-03-08 08:08:40 -0700

    steps 2-5 done

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const mongodb = require("./data/database");
+
 const app = express();
 
 const port = process.env.port || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => console.log(`Running on port: ${port}`));
+mongodb.initDb((err) => {
+    if(err) {
+        console.error(err);
+    } else {
+        app.listen(port, () => console.log(`Database listening and node running on port: ${port}`));
+    }
+});
